dde223a5eb86284855eb9e01ab14e0384fac22f1
Max Muth <max@blueshoe.de>
2017-06-27 15:04:23 +0200

    Add font-awesome icons to the hide on device setting

--- layouter/forms.py
+++ layouter/forms.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.forms.widgets import RadioSelect
+from django.forms.utils import flatatt
+from django.forms.widgets import RadioSelect, ChoiceInput, RadioFieldRenderer, CheckboxInput
 from layouter.models import ContainerPlugin
+from django.utils.encoding import force_text
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 class ButtonSelectWidget(RadioSelect):
@@ -21,12 +25,28 @@ class ButtonSelectWidget(RadioSelect):
         return o
 
 
+class DeviceIconCheckBoxWidget(CheckboxInput):
+    ICON_MAPPING = {
+        'disable_on_mobile': '<i class="fa fa-mobile" aria-hidden="true"></i>',
+        'disable_on_tablet': '<i class="fa fa-tablet" aria-hidden="true"></i>',
+        'disable_on_desktop': '<i class="fa fa-desktop" aria-hidden="true"></i>',
+    }
+
+    def render(self, name, value, attrs=None):
+        rendered_input = super(DeviceIconCheckBoxWidget, self).render(name, value, attrs)
+        rendered_icon = mark_safe(self.ICON_MAPPING.get(name))
+        return '{}{} '.format(rendered_input, rendered_icon)
+
+
 class ContainerPluginForm(forms.ModelForm):
     class Meta:
         model = ContainerPlugin
         fields = ['container_type']
         widgets = {
-            'container_type': ButtonSelectWidget
+            'container_type': ButtonSelectWidget,
+            'disable_on_mobile': DeviceIconCheckBoxWidget,
+            'disable_on_tablet': DeviceIconCheckBoxWidget,
+            'disable_on_desktop': DeviceIconCheckBoxWidget,
         }
 
     def clean(self):
