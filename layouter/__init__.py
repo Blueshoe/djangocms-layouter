371a38bf2c27a26b06248cdc70859b7442be6120
Robert Stein <robert@blueshoe.de>
2020-04-28 12:18:14 +0200

    - bump version - update deps

--- layouter/__init__.py
+++ layouter/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.3.3'
+__version__ = '1.0.0'
 default_app_config = 'layouter.apps.LayouterConfig'
--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development :: Libraries :: Python Modules',
@@ -26,7 +28,7 @@ CLASSIFIERS = [
 ]
 
 REQUIREMENTS = [
-    'django>=1.11,<2.0',
+    'django>=2,<3',
     'django-cms>=3.4.0',
     'easy-thumbnails>=2.1 ',
 ]
@@ -41,7 +43,7 @@ setup(
     author='Robert Stein',
     author_email='robert@blueshoe.de',
     url='https://github.com/Blueshoe/djangocms-layouter',
-    download_url='https://github.com/Blueshoe/djangocms-layouter/archive/0.3.1.zip',
+    download_url='https://github.com/Blueshoe/djangocms-layouter/archive/1.0.0.zip',
     install_requires=REQUIREMENTS,
     keywords=['django', 'Django CMS', 'grid', 'bootstrap', 'website', 'CMS', 'Blueshoe'],
     classifiers=CLASSIFIERS,
